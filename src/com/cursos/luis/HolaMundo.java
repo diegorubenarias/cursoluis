38ad347cc844ebc3984e61441cecf4a6d8423528
darias <darias@itx.com>
2021-07-13 19:11:50 -0300

    first commit

---
+++ src/com/cursos/luis/HolaMundo.java
@@ -0,0 +1,18 @@
+package com.cursos.luis;
+
+import com.cursos.luis.utils.StringUtils;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StringUtils.leerFrase().substring(4,5));
+		
+
+	}
+
+}
---
+++ src/com/cursos/luis/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.cursos.luis.utils;
+
+import java.util.Scanner;
+
+public class StringUtils {
+	
+	private StringUtils() {
+		
+	}
+	
+	public static String leerFrase() {
+		return Input.getInstance().leerFrase();
+	}
+	
+	
+}
